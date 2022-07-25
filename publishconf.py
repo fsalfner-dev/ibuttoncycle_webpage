d03dbe64aa748bca0b737e4851de155149db4d22
F S <fs.berlin67@gmail.com>
2022-07-25 08:46:19 +0200

    fixing public urls

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = 'https://fs-berlin67.github.io/biolog-website/'
-RELATIVE_URLS = False
+RELATIVE_URLS = True
 
 FEED_ALL_ATOM = ''
 CATEGORY_FEED_ATOM = ''
